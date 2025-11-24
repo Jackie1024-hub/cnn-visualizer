38565c20ab24998e4c1241f73699af58589951ac
Jackie1024-hub <shiziriji1024@163.com>
2025-11-24 11:05:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← 关键配置！让 Netlify 能正确加载文件
+  base: './',   // 关键修复！！！让 CSS 和图片路径正常
 })
